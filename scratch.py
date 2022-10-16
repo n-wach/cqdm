ff6ca2177cc59e5b4d7c7125bed29a0b3e6ff557
n-wach <nmwachholz@gmail.com>
2022-10-16 16:47:33 -0700

    Add native yielder that acts as iterable-passthrough

--- scratch.py
+++ scratch.py
@@ -5,14 +5,19 @@ import timeit
 
 def test_tqdm(n):
     def loop(_n):
-        for _ in tqdm.tqdm(range(_n)):
-            pass
+        total = 0
+        for i in tqdm.tqdm(range(_n)):
+            total += i
+        print(total)
     print(f"tqdm@{n}:", timeit.timeit(lambda: loop(n), number=3))
 
 
 def test_tqdmc(n):
     def loop(_n):
-        tqdmc.count(range(n))
+        total = 0
+        for i in tqdmc.yielder(range(_n)):
+            total += i
+        print(total)
     print(f"tqdm-c@{n}:", timeit.timeit(lambda: loop(n), number=3))
 
 
