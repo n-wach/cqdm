d43e852d2d494f1124e574a932b73d711fd0223d
n-wach <nmwachholz@gmail.com>
2022-10-16 16:19:01 -0700

    Add function for looping through an iterable

--- scratch.py
+++ scratch.py
@@ -1,3 +1,21 @@
 import tqdmc
+import tqdm
 
-tqdmc.print("hello fd 1")
+import timeit
+
+def test_tqdm(n):
+    def loop(_n):
+        for _ in tqdm.tqdm(range(_n)):
+            pass
+    print(f"tqdm@{n}:", timeit.timeit(lambda: loop(n), number=3))
+
+
+def test_tqdmc(n):
+    def loop(_n):
+        tqdmc.count(range(n))
+    print(f"tqdm-c@{n}:", timeit.timeit(lambda: loop(n), number=3))
+
+
+N = 10_000_000
+test_tqdm(N)
+test_tqdmc(N)
