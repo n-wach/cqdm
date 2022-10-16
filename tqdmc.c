512b074adaad163c32dd2c56e41cc75a3fa908dd
n-wach <nmwachholz@gmail.com>
2022-10-16 16:19:01 -0700

    Add function for looping through an iterable

--- tqdmc.c
+++ tqdmc.c
@@ -2,17 +2,45 @@
 
 static PyObject * tqdmc_print(PyObject *self, PyObject *args) {
     const char *command;
-    int sts;
 
     if (!PyArg_ParseTuple(args, "s", &command))
         return NULL;
+
     write(1, command, strlen(command));
-    return PyLong_FromLong(0);
+
+    Py_INCREF(Py_None);
+    return Py_None;
+}
+
+static PyObject * tqdmc_count(PyObject *self, PyObject *args) {
+    PyObject *obj;
+    int count = 0;
+
+    if (!PyArg_ParseTuple(args, "O", &obj))
+        return NULL;
+
+    PyObject *iterator = PyObject_GetIter(obj);
+    PyObject *item;
+
+    if (iterator == NULL) {
+        return NULL;
+    }
+
+    while ((item = PyIter_Next(iterator))) {
+        count++;
+        Py_DECREF(item);
+    }
+
+    Py_DECREF(iterator);
+
+    return PyLong_FromLong(count);
 }
 
 static PyMethodDef tqdmcMethods[] = {
     {"print",  tqdmc_print, METH_VARARGS,
      "Print a string to stdout"},
+    {"count",  tqdmc_count, METH_VARARGS,
+     "Iterate an iterable and return count"},
     {NULL, NULL, 0, NULL}        /* Sentinel */
 };
 
