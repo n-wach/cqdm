a423a54b85fc3f0870ed64df2f0196775764ee25
n-wach <nmwachholz@gmail.com>
2022-11-07 15:36:11 -0800

    Overwrite __setattr__ so monitor can update miniters

--- cqdm/__init__.py
+++ cqdm/__init__.py
@@ -3,10 +3,16 @@ import cqdm_native
 
 
 class cqdm(tqdm.tqdm):
+    def __setattr__(self, key, value):
+        super(cqdm, self).__setattr__(key, value)
+        if key == "miniters" and hasattr(self, "_cqdm"):
+            self._cqdm.set_miniters(value)
+
     def __iter__(self):
         if self.disable:
             yield from self.iterable
             return
 
-        self.cqdm = cqdm_native.cqdm(self)
-        yield from self.cqdm
+        self._cqdm = cqdm_native.cqdm(self)
+        yield from self._cqdm
+        del self._cqdm
