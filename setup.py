87c3c25ba7c56e4d2f66ad0b23b5ebf8ec60e1c9
n-wach <nmwachholz@gmail.com>
2022-11-07 16:15:10 -0800

    Deploy to PyPI

--- setup.py
+++ setup.py
@@ -1,15 +1,27 @@
-from distutils.core import setup, Extension
+from setuptools import Extension, find_packages, setup
 
 cqdm_native = Extension(
     "cqdm_native",
     sources=["cqdm.c"],
 )
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="cqdm",
-    version="1.0",
+    version="1.0.2",
     description="A drop-in replacement for the popular tqdm library, accelerated with C bindings",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/n-wach/cqdm",
     ext_modules=[cqdm_native],
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
     install_requires=["tqdm"],
 )
