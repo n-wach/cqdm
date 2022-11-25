3ee51701425b91308e1b4985aca26550f9f74e25
n-wach <nmwachholz@gmail.com>
2022-11-24 22:33:21 -0800

    Document release tagging

--- releasing/release.sh
+++ releasing/release.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+echo "Remember to tag and push the current commit!"
+# for instance:
+# git tag -a 1.0.4 "Release 1.0.4"
+# git push origin --tags
+
 # build wheels
 docker run --rm -e PLAT=manylinux2010_x86_64 -v `pwd`:/io quay.io/pypa/manylinux2010_x86_64 /io/releasing/build_wheels.sh
 
